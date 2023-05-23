f1a34929b925836ff5ef7cddc12a9cd967ac0ea2
unknown <juanmartin.pilon@gmail.com>
2023-05-23 15:11:22 -0300

    Aplicacion terminada, conectada correctamente con API Ninja y conectada correctamente con Twitter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   const [quote, setQuote] = useState('');
@@ -9,7 +11,7 @@ function App() {
   
   const getRandomQuote = async () => {
     try {
-      const response = await axios.get("https://api.api-ninjas.com/v1/quotes?category=hope", {
+      const response = await axios.get("https://api.api-ninjas.com/v1/quotes?category=inspirational", {
         headers: {
           'X-Api-Key': key
         },
@@ -27,7 +29,8 @@ function App() {
   };
 
   const tweetQuote = () => {
-    // Implementa lógica para compartir la cita en Twitter
+      const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote)} - ${encodeURIComponent(autor)}`;
+      window.open(twitterUrl, "_blank");
   };
 
   return (
@@ -35,16 +38,16 @@ function App() {
       <div id='box'>
         <h1>Random Quote Machine!</h1>
         <div id='quote'>
-          <p>{quote}</p>
+          <p>"{quote}"</p>
         </div>
         <div id='autor'>
-          <h3>{autor}</h3>
+          <h3>- {autor}</h3>
         </div>
         <button id='new-quote' onClick={handleNewQuote}>
           New Quote!
         </button>
-        <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${quote} - ${autor}`} target="_blank" rel="noopener noreferrer">
-          Share on Twitter!
+        <a id='tweet-quote' href="#" onClick={tweetQuote} target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-twitter"></i> Share on Twitter!
         </a>
       </div>
     </div>
